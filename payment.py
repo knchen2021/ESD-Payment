cb78f7c443d0d65bd56f8a78cceded159144e1af
knchen2021 <knchen.2021@scis.smu.edu.sg>
2023-04-06 17:23:03 +0800

    change port num

--- payment.py
+++ payment.py
@@ -77,4 +77,4 @@ def payment():
     return jsonify({"status": "error"}),500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5010, debug=True)
